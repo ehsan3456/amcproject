c7b1b69371d22f18dbc549b5b26136e38e00cc12
M. Ehsan Ul Haq <132472055+ehsan3456@users.noreply.github.com>
2025-05-04 02:53:43 +0500

    Update Analytics.js

--- sms_app/src/components/Analytics.js
+++ sms_app/src/components/Analytics.js
@@ -17,7 +17,7 @@ const Analytics = () => {
         
         const successRateResponse = await api.get('/analytics/success-rate/');
         setSuccessRate([
-          { name: 'Sent', value: successRateResponse.data.send },
+          { name: 'Sent', value: successRateResponse.data.sent },
           { name: 'Failed', value: successRateResponse.data.failed }
         ]);
         
